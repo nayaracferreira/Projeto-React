80132896f9734891a3d0dd0337c516dbc6e65bc9
Nayara do Carmo Ferreira <nayara.dcf@gmail.com>
2022-08-31 22:16:48 -0300

    Retirando a string na linha 8

--- src/components/molecules/UserListItem.jsx
+++ src/components/molecules/UserListItem.jsx
@@ -5,7 +5,7 @@ import Avatar from "../atoms/Avatar";
 
 export default function UserListItem(props) {
   return (
-    <Link to={"/users/${props.id}"} className="users__list-item">
+    <Link to={`/users/${props.id}`} className="users__list-item">
       <div className="users__list-item-photo">
         <Avatar src={props.src} />
       </div>
